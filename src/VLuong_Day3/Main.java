24f1ee73149bbe23aa393bcf774b26d1a55da3dd
VuLuong <huyvu2509@gmail.com>
2023-03-26 18:33:40 +0700

    Update ex Day 3

--- src/VLuong_Day3/Main.java
+++ src/VLuong_Day3/Main.java
@@ -1,36 +1,92 @@
 package VLuong_Day3;
 
+import java.lang.invoke.SwitchPoint;
+import java.util.Random;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
-public class Main {
+public class Main implements Runnable {
+	
+	private String browser;
+    private WebDriver driver;
+    
+    public Main(String browser) {
+		this.browser = browser;
+	}
+	
+    
 	public static void main(String[] args) {
-//		ChromeOptions option = new ChromeOptions();
-//		option.addArguments("--remote-allow-origins=*");
-//		WebDriver ChromeDr = new ChromeDriver(option);
-		WebDriver FDr = new FirefoxDriver();
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
 		
+		// Run Step by step dynamic browser
+		WebDriver EdgeDr = new EdgeDriver();
+		WebDriver FFDr = new FirefoxDriver();
+		WebDriver ChromeDr = new ChromeDriver(option);				
+		Process(ChromeDr);
+		Process(FFDr);
+		Process(EdgeDr);
 		
+		// Multi run all Browser at the same time
+//		Thread EdgeDr = new Thread(new Main("Edge"));
+//		Thread FFDr = new Thread(new Main("FireFox"));
+//		Thread ChromeDr = new Thread(new Main("Chrome"));
+//		EdgeDr.start();
+//		FFDr.start();
+//		ChromeDr.start();
+	}
+	
+	@Override
+	public void run() {
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+		
+		// TODO Auto-generated method stub
+		if (browser.equals("FireFox")) {
+            driver = new FirefoxDriver();
+            Process(driver);
+        } else if (browser.equals("Edge")) {
+            driver = new EdgeDriver();
+            Process(driver);
+        } else if (browser.equals("Chrome")) {
+            driver = new ChromeDriver(option);
+            Process(driver);
+        } else {
+            throw new IllegalArgumentException("Invalid browser: " + browser);
+        }
+	}
+	
+	public static void Process(WebDriver curFdr) {
+		curFdr.manage().window().maximize();
+		
+		// xpath for element
 		String URL = "https://demo.guru99.com/v4/";
+		String xpathUid= "//tbody/tr[4]/td[2]";
+		String xpathP= "//tbody/tr[5]/td[2]";
 		String xpathclickhere = "//a[@href='http://demo.guru99.com/']";
 		String xpathEmail = "//input[@name='emailid']";
 		String xpathID = "//input[@name='uid']";
 		String xpathPass = "//input[@name='password']";
 		String xpathLog = "//input[@type='submit']";
 		String xpathWel = "//marquee[@class='heading3']";
-		String username = "mngr487896";
-		String userpass = "mypevem";
 		String xpathbtnSubmitemail = "//input[@name='btnLogin']";
 		String ExpectSubmit = "Please take SCREESNSHOT of this page for future reference.";
 		String ExpectLogin = "Welcome To Manager's Page of Guru99 Bank";
 		String xpathNotiSubmit = "//h4";
 		
-		FDr.get(URL);
+		// Random email 
+		Random ran = new Random();
+		String email = "huyvu" + ran.nextInt(2000) + "@gmail.com";
 		
+		
+		curFdr.get(URL);	
 		/*TC1
 		 * Click in click here hyperlink
 		 * Enter email
@@ -38,24 +94,46 @@ public class Main {
 		 * Submit successful
 		 * TC PASS
 		*/
-		
-		WebElement BtnClickhere = FDr.findElement(By.xpath(xpathclickhere));
+		// Click HERE hyper link
+		WebElement BtnClickhere = curFdr.findElement(By.xpath(xpathclickhere));
 		BtnClickhere.click();
 		
-		WebElement myemail = FDr.findElement(By.xpath(xpathEmail));
-		myemail.sendKeys("huyvu2509@gmail.com");
+		// By pass iframe and close ads
+		String url_ads = "https://demo.guru99.com/v4/#google_vignette";
+		String current_URL = curFdr.getCurrentUrl();
+		if (current_URL.equalsIgnoreCase(url_ads)) {
+			WebElement ifr_ads = curFdr.findElement(By.xpath("//iframe[contains(@id,'google_ads_iframe_')]"));
+			curFdr.switchTo().frame(ifr_ads);
+			WebElement close_ads = curFdr.findElement(By.xpath("//div[@id='dismiss-button']"));
+			close_ads.click();
+			curFdr.switchTo().defaultContent();			
+		}
 		
-		WebElement SubmitBtn = FDr.findElement(By.xpath(xpathbtnSubmitemail));
+		// Enter email 
+		WebElement myemail = curFdr.findElement(By.xpath(xpathEmail));
+		myemail.sendKeys(email);
+		// CLick Submit
+		WebElement SubmitBtn = curFdr.findElement(By.xpath(xpathbtnSubmitemail));
 		SubmitBtn.click();
-		
-		WebElement NotiSubmit = FDr.findElement(By.xpath(xpathNotiSubmit));
+		// Get text notify then submit
+		WebElement NotiSubmit = curFdr.findElement(By.xpath(xpathNotiSubmit));
 		String Sub_actual = NotiSubmit.getText();
 		
+		// Compare and verify notify
 		if (Sub_actual.equalsIgnoreCase(ExpectSubmit)) {
-			System.out.println("TC Submit Thanh Cong");
+			System.out.println("TestCase Submit successful");
 		} else {
-			System.out.println("TC Submit That Bai");
+			System.out.println("TestCase Submit successful");
 		}
+		
+		
+		WebElement idtext = curFdr.findElement(By.xpath(xpathUid));
+		// Get ID in page 
+		String ID = idtext.getText();
+		
+		WebElement passtext = curFdr.findElement(By.xpath(xpathP));
+		// Get PASS in Page
+		String PASS = passtext.getText();
 		
 		/*TC2
 		 * Access "https://demo.guru99.com/v4/
@@ -66,28 +144,33 @@ public class Main {
 		 * TC PASS
 		*/
 		
-		FDr.get(URL);
+		curFdr.navigate().to(URL);
 		
-		WebElement userID = FDr.findElement(By.xpath(xpathID));
-		userID.sendKeys(username);
 		
-		WebElement userPass = FDr.findElement(By.xpath(xpathPass));
-		userPass.sendKeys(userpass);
+		WebElement userID = curFdr.findElement(By.xpath(xpathID));
+		userID.sendKeys(ID);
+			
+		WebElement userPass = curFdr.findElement(By.xpath(xpathPass));
+		userPass.sendKeys(PASS);
 		
-		WebElement Login = FDr.findElement(By.xpath(xpathLog));
+		WebElement Login = curFdr.findElement(By.xpath(xpathLog));
 		Login.click();
 		
-		WebElement NotiLog = FDr.findElement(By.xpath(xpathWel));
+		WebElement NotiLog = curFdr.findElement(By.xpath(xpathWel));
 		String Wel_actual = NotiLog.getText();
 		
 		if (Wel_actual.equalsIgnoreCase(ExpectLogin)) {
-			System.out.println("TC Login Thanh Cong");
+			System.out.println("TestCase Login successful");
 		} else {
-			System.out.println("TC Login That Bai");
+			System.out.println("TestCase Login successful");
 		}
 
+			
 		
-		FDr.quit();
+		
+		curFdr.quit();
 	}
+
+	
 
 }
